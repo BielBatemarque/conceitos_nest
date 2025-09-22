3d5c768b7a6b6463f4e03eaa379d1d20b1666b4c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-09-22 15:57:23 -0300

    feat: configurando validação dos dados do dto

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,28 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateRecadoDto {
-    readonly texto: string;
-    readonly de: string;
-    readonly para: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
+  @IsOptional()
+  readonly texto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  readonly para: string;
 }
