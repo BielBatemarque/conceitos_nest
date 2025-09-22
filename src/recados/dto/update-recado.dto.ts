533230d9923b65d9bdd338ab47a0f3f4391be7c8
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-09-22 16:26:10 -0300

    criando tipo parcial para dto de atualização

--- src/recados/dto/update-recado.dto.ts
+++ src/recados/dto/update-recado.dto.ts
@@ -1,5 +1,16 @@
-export class UpdateRecadoDto {
-    readonly texto?: string;
-    readonly de?: string;
-    readonly para?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateRecadoDto } from './create-recado.dto';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
+  @IsBoolean()
+  @IsOptional()
+  readonly lido?: boolean;
 }
