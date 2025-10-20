6c44f5684335ca6d380597a9a52e6e626ef5eaa9
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-10-20 16:02:37 -0300

    Feat: configurações de conexão com base da dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5430,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true, //Carrega entidades sem precisar especifica-las
+      synchronize: true // Não deve ser utsado em produção
+  }), 
+  RecadosModule
+],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
