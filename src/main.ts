e39f2c0022abfe0af8551c83baf1f550220c80d7
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-10-20 15:01:04 -0300

    Fix: ajustando a segurança das requests configurando whitelist

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove chaves que não estão no dto
+    forbidNonWhitelisted: true, //levanta erro quando a chave não existir
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
