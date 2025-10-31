16b61ff8f35a011a8909ac04e983f6f99b96dd4e
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-10-31 13:31:47 -0300

    Feat: criando interceptors para adicionar um header a response

---
+++ src/common/interceptors/add-header.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+export class AddHeaderInterceptor implements NestInterceptor{
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        const response = context.switchToHttp().getResponse();
+
+        response.setHeader('x-custom-header', 'O VALOR DO MEU CABEÇALHO');
+        return next.handle();
+    }
+}
--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -10,6 +10,7 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
   UsePipes,
 } from '@nestjs/common';
 import { RecadosService } from './recados.service';
@@ -17,6 +18,7 @@ import { CreateRecadoDto } from './dto/create-recado.dto';
 import { UpdateRecadoDto } from './dto/update-recado.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
+import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 
 @Controller('recados')
 export class RecadosController {
@@ -24,6 +26,7 @@ export class RecadosController {
 
   @HttpCode(HttpStatus.OK)
   @Get()
+  @UseInterceptors(AddHeaderInterceptor)
   findAll(@Query() paginationDto: PaginationDto) {
     return this.recadosService.findAll(paginationDto);
   }
